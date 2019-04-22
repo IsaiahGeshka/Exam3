cc471f95c80db48825d0c428744881347741c99c
IsaiahGeshka <42741552+IsaiahGeshka@users.noreply.github.com>
2019-04-22 13:15:58 -0500

    Add files via upload
    Exam 3 Javascrript

---
+++ exam3/exam3.js
@@ -0,0 +1,15 @@
+// JSON.parse(data);
+var data = require('./data.json');
+
+
+// var da =  {
+//     "name": fname,
+//     "age": age
+// }
+
+
+console.log(data.items[0])
+console.log(data.items[1])
+
+
+
